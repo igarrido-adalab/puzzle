1537f783feb85dbb3ea127eefe9aed044f943b09
Iván Garrido (Adalab professor) <ivan@adalab.es>
2024-02-27 11:49:50 +0100

    Some docs.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,6 +6,12 @@ import Contact from './Contact'
 import Error404 from './Error404'
 import { Route, Routes } from 'react-router-dom'
 
+/*
+ - .nojekyll: [Configuring a publishing source for your GitHub Pages site](https://docs.github.com/en/pages/getting-started-with-github-pages/configuring-a-publishing-source-for-your-github-pages-site#troubleshooting-publishing-from-a-branch)
+ - 404.html:  [Crear una página 404 personalizada para tu sitio de Páginas de GitHub](https://docs.github.com/es/pages/getting-started-with-github-pages/creating-a-custom-404-page-for-your-github-pages-site)
+ - basename:  [BrowserRouter basename property](https://reactrouter.com/en/main/router-components/browser-router#basename)
+*/
+
 function App() {
   return (
     <div>
